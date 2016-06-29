2c5409e710fe694f93dd0a44b70bc8cf6e5abcb4
Darkcacht <iln@outlook.com.ar>
2016-06-29 11:47:50 -0300

    Update ejlogica3.py

--- ejlogica3.py
+++ ejlogica3.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import os
+clear = lambda: os.system('clear')
+clear()
+
 for i in range(1, 7):
     for j in range(i, 7):
         print("{}:{}".format(i, j))
